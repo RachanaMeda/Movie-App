df36f4ffecb6050393d99562871e4d76844dd0c7
RachanaMeda <123768879+RachanaMeda@users.noreply.github.com>
2023-03-24 00:43:22 +0530

    Update script.js

--- Movie-App/script.js
+++ Movie-App/script.js
@@ -4,7 +4,7 @@ const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const searchURL = BASE_URL + "/search/movie?" + API_KEY;
 
-const genres = [
+const genres = 
   {
     id: 28,
     name: "Action",
